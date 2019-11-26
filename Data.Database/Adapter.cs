3bb0d441d9a4be4758f96e2e7fe5fd89ca3ed7fb
camvives <camvives@gmail.com>
2019-11-25 22:16:05 -0300

    Clase Adapter

--- Data.Database/Adapter.cs
+++ Data.Database/Adapter.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace Data.Database
 {
     public class Adapter
     {
+        public SqlConnection sqlConn = new SqlConnection();
+
+        protected void OpenConnection()
+        {
+            string conn = ConfigurationManager.ConnectionStrings["ExpressConnection"].ConnectionString;
+            sqlConn = new SqlConnection(conn);
+            sqlConn.Open();
+        }
+
+        protected void CloseConnection()
+        {
+            sqlConn.Close();
+            sqlConn = null;
+        }
     }
 }
