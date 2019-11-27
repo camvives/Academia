ea931240c825277ebad2387ca7b0c8462cecd11f
camvives <camvives@gmail.com>
2019-11-26 22:37:25 -0300

    Metodo GetAll en Especialidades

--- Business.Logic/Especialidad_Logic.cs
+++ Business.Logic/Especialidad_Logic.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Entities;
+using Data.Database;
+
 
 namespace Business.Logic
 {
     public class Especialidad_Logic
     {
+        public EspecialidadAdapter EspecialidadData { get; set; }
+
         public List<Especialidad> GetAll()
         {
             EspecialidadData.GetAll();
