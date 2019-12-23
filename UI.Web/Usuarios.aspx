6d500d4acdd9d06192a181b2f006c6cf8c5a1012
camvives <camvives@gmail.com>
2019-12-23 10:54:43 -0300

    gridView Usuarios Web

---
+++ UI.Web/Usuarios.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="Usuarios.aspx.cs" Inherits="UI.Web.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gridView" runat="server" AutoGenerateColumns = "False" DataKeyNames="ID">
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" />
+                <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="Apellido" HeaderText="Apellido" />
+                <asp:BoundField DataField="NombreUsuario" HeaderText="Nombre de Usuario" />
+                <asp:BoundField DataField="Email" HeaderText="Email" />
+                <asp:CheckBoxField DataField="Habilitado" HeaderText="Habilitado" />
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+</asp:Content>
---
+++ UI.Web/Usuarios.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Logic;
+using Business.Entities;
+using System.Data;
+
+
+namespace UI.Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            this.CompletarGrid();
+        }
+
+        public void CompletarGrid()
+        {
+            
+            UsuarioLogic ul = new UsuarioLogic();
+            List<Persona> personas;
+            List<Business.Entities.Usuario> usuarios;
+            (usuarios, personas) = ul.GetAll();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Nombre");
+            dt.Columns.Add("Apellido");
+            dt.Columns.Add("NombreUsuario");
+            dt.Columns.Add("Email");
+            dt.Columns.Add("Habilitado");
+
+            foreach (var usr in personas.Zip(usuarios, (a, b) => new { a, b }))  //Linq combina las dos listas
+            {
+                dt.Rows.Add(usr.b.ID, usr.a.Nombre, usr.a.Apellido, usr.b.NombreUsuario, usr.a.Email, usr.b.Habilitado);
+            }
+
+            gridView.DataSource = dt;
+            gridView.DataBind();
+        }
+
+    }
+}
