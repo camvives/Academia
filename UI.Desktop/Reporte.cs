e91a7d5db897aaa2bbbdff9e4602d148af9ba673
camvives <camvives@gmail.com>
2020-01-26 23:12:19 -0300

    Reporte

---
+++ UI.Desktop/Reporte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Util;
+
+namespace UI.Desktop
+{
+    public partial class Reporte : Form
+    {
+        public Reporte()
+        {
+            InitializeComponent();
+        }
+
+        private void Reporte_Load(object sender, EventArgs e)
+        {
+            Certificado_Inscripcion reporte = new Certificado_Inscripcion();
+            reporte.SetParameterValue("@ID_PERSONA", "94");
+            crystalReportViewer1.ReportSource = reporte;
+        }
+    }
+}
